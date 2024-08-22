ee830fde386179bfbef4e8817a8f3d808dc1e75c
Ilia Shilkin <shilo1008@icloud.com>
2024-08-22 07:10:10 +0200

    New API

--- bot.js
+++ bot.js
@@ -2323,7 +2323,7 @@ async function checkFuelExpirations(data) {
         const day = String(date.getDate()).padStart(2, '0');
         const month = String(date.getMonth() + 1).padStart(2, '0'); // Месяцы начинаются с 0
         const year = date.getFullYear();
-        return `${day}/${month}/${year}`;
+        return `${day} ${month} ${year}`;
     };
 
     // Формирование сообщения для всех станций
